a369245690c0e16b4a414b196ae67cd611b01852
class <tsdhanush169@gmail.com>
2024-10-08 22:31:12 +0530

    fixed bugs

--- server/skinserver/api/urls.py
+++ server/skinserver/api/urls.py
@@ -3,5 +3,5 @@ from skinserver.api.views import UserListAV, Login
 
 urlpatterns = [
     path('signup/', UserListAV.as_view(), name='user-list'),
-    path('login/<int:pk>/', Login.as_view(), name='user-detail'),
+    path('login/', Login.as_view(), name='user-detail'),
 ]
